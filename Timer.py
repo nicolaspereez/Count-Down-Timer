677eb1a697424921fbcd16e7d3cf87da90c31732
nicolaspereez <92058498+nicolaspereez@users.noreply.github.com>
2022-01-08 13:46:01 -0300

    Add files via upload

---
+++ Timer.py
@@ -0,0 +1,13 @@
+import time
+
+def countdown(t):
+    while t:
+        mins,sec =divmod(t, 60)
+        timer = ('{:02d}:{:02d}').format(mins,sec)
+        print(timer, end='\r')
+        time.sleep(1)
+        t-=1
+    print('Time up!')
+
+t = int(input('Enter the number to countdown: '))
+countdown(t)
